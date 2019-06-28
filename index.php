7f0de3e744cb9a3070ab0e7ee64b42fe73bc97b3
Moritz Heidkamp <moritz.heidkamp@bevuta.com>
2019-06-28 10:19:19 +0200

    wat

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
  * @var bool
  */
 define('WP_USE_THEMES', false);
+define('wat', 'yes');
 
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
